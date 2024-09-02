8d3c12e3e34b5c07c7bb588639ac613fe0ca3e13
t0l9 <t0l4ik.kas1@gmail.com>
2024-09-02 21:59:25 +0500

    new lesson

---
+++ src/test/java/simple/SkipTests.java
@@ -0,0 +1,23 @@
+package simple;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@Tag("simple")
+public class SkipTests {
+
+    @Test
+    @Disabled("Выключен")
+    void test1(){
+        assertTrue(true);
+    }
+
+    @Disabled("Причина")
+    @Test
+    void test2(){
+        assertTrue(true);
+    }
+}
